ad0475def4510f7d33ce9021cf3b8b927e88c119
AuLu <aluthituyet@openwt.com>
2021-08-09 18:21:28 +0700

    Add Facebook Pixel Plugin in Jobs

--- web/themes/custom/openwt2018/assets/scripts/event-webform.js
+++ web/themes/custom/openwt2018/assets/scripts/event-webform.js
@@ -1,20 +1,12 @@
 (function ($, window, Drupal, drupalSettings) {
-
   
     Drupal.behaviors.addPeople = {
       attach: function (context, settings) {
-        var val = $( 'input[name=number_of_participants]:checked' ).val();
-        $('<span id="numbers" class="number-block">'+ val + '</span>').insertAfter(".form-item-number-of-participants:first-child");
-        
-        $('input[name=number_of_participants]').on("change",function(){
-          val = $( 'input[name=number_of_participants]:checked' ).val();
-          $('#numbers').text(val);
-          $('input[name=participants]').val(val);
+        $('.btn-submit-apply').once('btnApplyClick').click(function () {
+          fbq('trackCustom', 'SubmitApplication', { job_id: $("#job-id").text() });
         });
-        
       }
     }
-    
   
-  })(jQuery, this, Drupal, drupalSettings);
+})(jQuery, this, Drupal, drupalSettings);
   
